1476933db2295677a3853e5f0fa970159ebc9381
laviniacavalet <116130863+laviniacavalet@users.noreply.github.com>
2024-04-16 05:48:05 +0000

    first attempt showing only currency pairs

--- app.rb
+++ app.rb
@@ -1,9 +1,48 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+require "json"
 
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
   <p>Define some routes in app.rb</p>
   "
+    # build the API url, including the API key in the query string
+    api_url = "http://api.exchangerate.host/list?access_key=#{ENV['EXCHANGE_RATES_KEY']}"
+    
+    #response = HTTP.get(api_url)
+    #@parsed = JSON.parse(response.to_s)
+
+    parsed_data = JSON.parse(HTTP.get(api_url).to_s)
+    @parsed = parsed_data["currencies"]
+    erb(:homepage)
 end
+
+get("/:currency") do
+  @original_currency = params.fetch("currency")
+  api_key = ENV['EXCHANGE_RATES_KEY']
+  
+  raw_data = HTTP.get("http://api.exchangerate.host/list?access_key=#{api_key}")
+  raw_data_string = raw_data.to_s
+  
+  parsed_data = JSON.parse(raw_data_string)
+  @parsed = parsed_data["currencies"]
+  
+  erb(:currency)
+  end
+
+  get("/:from_currency/:to_currency") do
+    api_key = ENV['EXCHANGE_RATES_KEY']
+    @original_currency = params.fetch("from_currency")
+    @destination_currency = params.fetch("to_currency")
+    
+    api_url = "http://api.exchangerate.host/convert?access_key=#{api_key}&from=#{@original_currency}&to=#{@destination_currency}&amount=1"
+    
+    aw_data = HTTP.get(api_url).to_s
+    
+    parsed_data = JSON.parse(raw_data_string)
+    @converted = parsed_data["currencies"]
+    
+    erb(:rate)
+    end
