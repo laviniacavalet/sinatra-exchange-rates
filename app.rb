e9dfa00db4aafd52d57f0aaf7b318f3e98cf70c4
laviniacavalet <116130863+laviniacavalet@users.noreply.github.com>
2024-04-16 16:24:48 +0000

    final version

--- app.rb
+++ app.rb
@@ -11,14 +11,27 @@ get("/") do
     #response = HTTP.get(api_url)
     #@parsed = JSON.parse(response.to_s)
 
-    @parsed_data = JSON.parse(HTTP.get(api_url).to_s)
-    @parsed = @parsed_data["currencies"]
+    parsed_data = JSON.parse(HTTP.get(api_url).to_s)
+    @parsed = parsed_data["currencies"]
+
     erb(:homepage)
 end
 
+
+=begin
+get("/:from_currency") do
+  api_key = (ENV["EXCHANGE_RATES_KEY"]).to_s
+  @original_currency = params.fetch("from_currency")
+
+  api_url = "https://api.exchangerate.host/list?access_key=#{api_key}"
+  
+  # some more code to parse the URL and render a view template
+end
+=end
+
 get("/:currency") do
   @original_currency = params.fetch("currency")
-  api_key = ENV['EXCHANGE_RATES_KEY']
+  api_key = (ENV["EXCHANGE_RATES_KEY"]).to_s
   
   raw_data = HTTP.get("http://api.exchangerate.host/list?access_key=#{api_key}")
   raw_data_string = raw_data.to_s
@@ -27,19 +40,20 @@ get("/:currency") do
   @parsed = parsed_data["currencies"]
   
   erb(:currency)
-  end
+  
+end
 
-  get("/:from_currency/:to_currency") do
-    api_key = ENV['EXCHANGE_RATES_KEY']
-    @original_currency = params.fetch("from_currency")
-    @destination_currency = params.fetch("to_currency")
-    
-    api_url = "http://api.exchangerate.host/convert?access_key=#{api_key}&from=#{@original_currency}&to=#{@destination_currency}&amount=1"
-    
-    aw_data = HTTP.get(api_url).to_s
-    
-    parsed_data = JSON.parse(raw_data_string)
-    @converted = parsed_data["currencies"]
-    
-    erb(:rate)
-    end
+get("/:currency/:to_currency") do
+api_key = (ENV["EXCHANGE_RATES_KEY"]).to_s
+@original_currency = params.fetch("currency")
+@destination_currency = params.fetch("to_currency")
+
+api_url = "http://api.exchangerate.host/convert?access_key=#{api_key}&from=#{@original_currency}&to=#{@destination_currency}&amount=1"
+
+raw_data = HTTP.get(api_url).to_s
+
+@parsed_data = JSON.parse(raw_data)
+@rate = @parsed_data["result"]
+
+erb(:rate)
+end
